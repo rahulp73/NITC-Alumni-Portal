e4a0fd09c24e08cb1a3c591dcb52349516ef496e
rahulp73 <rahulpothumudi6@gmail.com>
2025-10-16 19:26:05 +0530

    Added Sign-In Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import PrivateRoutes from './PrivateRoutes'
+import ProtectedRoutes from './ProtectedRoutes'
+import getAuthToken from './utils/getAuthToken';
+import SignIn from './pages/SignIn';
 import './App.css'
+import AppTheme from './components/shared-theme/AppTheme';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [authToken, setAuthToken] = useState(Boolean(getAuthToken()))
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <AppTheme>
+        <CssBaseline />
+        <Routes>
+          <Route element={<ProtectedRoutes authToken={authToken} />} >
+            <Route path="/signin" element={<SignIn setAuthToken={setAuthToken} />} />
+          </Route>
+          <Route element={<PrivateRoutes authToken={authToken} />}>
+            <Route path="/*" element={<h1>Private Routes</h1>} />
+          </Route>
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </AppTheme>
+    </BrowserRouter>
   )
 }
 
